96d435693205450cd058a83e5926ce801de07079
Hazimi <36977569+xhazimix@users.noreply.github.com>
2019-03-02 17:43:49 +0700

    revision

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,4 @@
 #! /bin/bash/
 
-cat /var/log/syslog | awk 'tolower($0) ~!/sudo/ && tolower($0) ~/cron/ {print $0;}' | awk 'NF < 13' >> /home/xhazimix/modul1/user.log
+tstmp = `date '+%Y-%m-%d_%H:%M'`
+cat /var/log/syslog | awk 'tolower($0) ~!/sudo/ && tolower($0) ~/cron/ {print $0;}' | awk 'NF < 13' >> ~/home/xhazimix/modul1/syslog_$tstmp.log
